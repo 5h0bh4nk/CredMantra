df0067ad730526864674cdc993739c086cc4f237
Shubhank <kul.shubhank207@gmail.com>
2023-01-21 23:59:14 +0530

    responsiveness improved

--- src/app/pages/team-page/team-page.component.ts
+++ src/app/pages/team-page/team-page.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+export interface TeamMember {
+  name: string;
+  position: string;
+  description: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-team-page',
   templateUrl: './team-page.component.html',
   styleUrls: ['./team-page.component.css']
 })
 export class TeamPageComponent {
+  team: TeamMember[] = [
+    {
+      name: 'John Doe',
+      position: 'CEO',
+      description: 'lorem lorem',
+      image: '../../../assets/team_member.png'
+    },
+    {
+      name: 'John Doe',
+      position: 'CEO',
+      description: 'lorem lorem',
+      image: '../../../assets/team_member.png'
+    },
+    {
+      name: 'John Doe',
+      position: 'CEO',
+      description: 'lorem lorem',
+      image: '../../../assets/team_member.png'
+    },
+    {
+      name: 'John Doe',
+      position: 'CEO',
+      description: 'lorem lorem',
+      image: '../../../assets/team_member.png'
+    }
+  ];
 
 }
